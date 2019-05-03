60ff5f039c450ebd17c3309d05b7ac07a38688e5
mikey <manicdesigner@gmail.com>
2019-05-02 22:03:37 -0400

    feat(index.js) mouseover and mouseout change to a color on hover and black on leave

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // Your code goes here
+
+/**
+ * 1. mouseover
+ */
+const navLinks = document.querySelectorAll('.nav-link');
+navLinks.forEach(navLink => {
+    navLink.addEventListener('mouseover', e => {
+        e.target.style.color = '#17A2B8';
+    })
+});
+/**
+ * 1. A -> mouseout
+ */
+navLinks.forEach(navLink => {
+    navLink.addEventListener('mouseout', event => {
+        event.target.style.color = 'black';
+    });
+});
+/**
+ * 
+ */
