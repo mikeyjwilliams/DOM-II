7f6c9cc8a57c8dd0ba626df7d47ad61e86c59c9d
Michael Williams <manicdesigner@gmail.com>
2019-05-13 10:57:08 -0400

    refactor(index.js) mv items and comment them out

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,11 +1,19 @@
 
-const blocksSection = document.querySelector('.blocks');
+const allBlocks = blocksSection.querySelectorAll('.block'); // class on all block items
+const blocksSection = document.querySelector('.blocks'); // class around all blocks.
+
 const redBlock = blocksSection.querySelector('.block--red');
 const blueBlock = blocksSection.querySelector('.block--blue');
 const greenBlock = blocksSection.querySelector('.block--green');
 const pinkBlock = blocksSection.querySelector('.block--pink');
 const grayBlock = blocksSection.querySelector('.block--gray');
 
-const allBlocks = blocksSection.querySelectorAll('.block');
 
-
+allBlocks.forEach(block => {
+    
+    block.addEventListener('click', e => {
+        if(e.target !== blocksSection.firstElementChild) {
+            blocksSection.prepend(e.target);
+        }
+    })
+});
