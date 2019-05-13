8d131c449cb36ad4f5a941ecd203dff85d62c038
Michael Williams <manicdesigner@gmail.com>
2019-05-13 10:18:26 -0400

    feat(index.js core) select all blocks individually by .blocks selector

---
+++ stretch-assignment/index.js
@@ -0,0 +1,11 @@
+
+const blocksSection = document.querySelector('.blocks');
+const redBlock = blocksSection.querySelector('.block--red');
+const blueBlock = blocksSection.querySelector('.block--blue');
+const greenBlock = blocksSection.querySelector('.block--green');
+const pinkBlock = blocksSection.querySelector('.block--pink');
+const grayBlock = blocksSection.querySelector('.block--gray');
+
+const allBlocks = blocksSection.querySelectorAll('.block');
+
+
