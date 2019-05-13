ece37d5d40edb9001d6947b5aa4ca9f6d5dbc6f9
Michael Williams <manicdesigner@gmail.com>
2019-05-13 11:13:08 -0400

    feat(index.js) boxes and cat i think move up when clicked

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,19 +1,20 @@
 
-const allBlocks = blocksSection.querySelectorAll('.block'); // class on all block items
+const allBlocks = document.querySelectorAll('.block'); // class on all block items
 const blocksSection = document.querySelector('.blocks'); // class around all blocks.
 
-const redBlock = blocksSection.querySelector('.block--red');
-const blueBlock = blocksSection.querySelector('.block--blue');
-const greenBlock = blocksSection.querySelector('.block--green');
-const pinkBlock = blocksSection.querySelector('.block--pink');
-const grayBlock = blocksSection.querySelector('.block--gray');
+const redBlock = document.querySelector('.block--red');
+const blueBlock = document.querySelector('.block--blue');
+const greenBlock = document.querySelector('.block--green');
+const pinkBlock = document.querySelector('.block--pink');
+const grayBlock = document.querySelector('.block--gray');
 
 
 allBlocks.forEach(block => {
     
     block.addEventListener('click', e => {
         if(e.target !== blocksSection.firstElementChild) {
-            blocksSection.prepend(e.target);
+            blocksSection.prepend(e.currentTarget);
+            
         }
     })
 });
